c8d35813eba3d0336daf3c595cd42764c90b8a03
AIM94 <martyn.majer@gmail.com>
2016-04-17 22:39:39 +0200

    cleanup

--- scripts/partial_dist_matrix_all.sh
+++ scripts/partial_dist_matrix_all.sh
@@ -15,5 +15,5 @@ python partial_dist_matrix_cli.py $SAMPLES_DIR $FEATURES_DIR ste_10_10_norm.hdf5
 python partial_dist_matrix_cli.py $SAMPLES_DIR $FEATURES_DIR ste_sti_stzcr_10_10_norm_optim_single.hdf5 $OUTPUT_DIR --dist_func True --alpha 0.3 --beta 0.3 --gamma 0.4
 python partial_dist_matrix_cli.py $SAMPLES_DIR $FEATURES_DIR ste_sti_stzcr_10_10_norm_optim_all.hdf5 $OUTPUT_DIR --dist_func True --alpha 0.0 --beta 0.6 --gamma 0.4
 
-
-
+# bottleneck features
+python partial_dist_matrix_cli.py $SAMPLES_DIR $FEATURES_DIR BN3bip_MM_8.hdf5 BN3bip_MM_16a.hdf5 BN3bip_MM-SA_8.hdf5 BN3bip_MM-SA_16.hdf5 BN3bip_MM-SA_32.hdf5 $OUTPUT_DIR
